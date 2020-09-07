85b10b0e3919074efef2e4efb327d76a1b035754
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-09-06 23:37:54 -0300

    ArrayHelper

--- src/ArrayHelper.php
+++ src/ArrayHelper.php
@@ -18,4 +18,46 @@ class ArrayHelper
     ): string {
         return mb_strtolower(preg_replace('/(?<=\\w)([A-Z])/', "_" . '\\1', $str));
     }
+
+    /**
+     * Check if an $needle is subarray of $arr.
+     * 
+     * @param array $needle
+     * @param array $arr
+     * 
+     * @return bool
+     */
+    public static function ArrayContains(
+        array $needle,
+        array $arr
+    ): bool {
+        $c = 0;
+
+        foreach ($needle as $array) {
+            $c += (int) in_array($array, $arr) ? 1 : 0;
+        }
+
+        return (int) $c === (int) count($needle) ? true : false;
+    }
+
+    /**
+     * Method used to return an array of colors in hexadecimal format.
+     * 
+     * @param int $n
+     * 
+     * @return array
+     */
+    public static function getArrayOfHexColors(
+        int $n = 0
+    ): array {
+        if (
+            $n <= 0
+        ) {
+            throw new \InvalidArgumentException("The method getArrayOfHexColors expect receive an positive and not null parameter. :{$n} received.");
+        }
+
+        return array_map(function () {
+            return (new \Quark\StringHelper())->getHEXRandomColor();
+        }, array_fill(NULL, $n, NULL));
+    }
 }
--- tests/Unit/ArrayTest.php
+++ tests/Unit/ArrayTest.php
@@ -6,6 +6,9 @@ use PHPUnit\Framework\TestCase;
 
 class ArrayTest extends TestCase
 {
+    /** @var string */
+    private const STRICT_HEXADECIMAL_COLOR_REGEX = '/^#[\d\w+]{6}$/';
+
     /**
      * Underscore Test.
      * 
@@ -17,5 +20,47 @@ class ArrayTest extends TestCase
         $this->assertEquals((new \Quark\ArrayHelper())->underscore('Color'), 'color');
         $this->assertEquals((new \Quark\ArrayHelper())->underscore('PaymentMethod'), 'payment_method');
         $this->assertEquals((new \Quark\ArrayHelper())->underscore('FooBarGoo'), 'foo_bar_goo');
+    }
+
+    /**
+     * Test for obtaining an array of random colors in hexadecimal format.
+     * 
+     * @param void
+     */
+    public function testgetArrayOfHexColors()
+    {
+        $n = rand(2, 128);
+
+        $data = (new \Quark\ArrayHelper())->getArrayOfHexColors($n);
+
+        $this->assertCount($n, $data);
+
+        foreach ($data as $data) {
+            $this->assertTrue((bool)(preg_match(self::STRICT_HEXADECIMAL_COLOR_REGEX, $data)));
+        }
+    }
+
+    /**
+     * Test case where the method getArrayOfHexColors receive an negative value as argument.
+     * 
+     * @param void
+     */
+    public function testgetArrayOfHexColorsWithNegativeArgument()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        (new \Quark\ArrayHelper())->getArrayOfHexColors(-1);
+    }
+
+    /**
+     * Test case where the method getArrayOfHexColors receive an null value as argument.
+     * 
+     * @param void
+     */
+    public function testgetArrayOfHexColorsWithZeroasArgument()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        (new \Quark\ArrayHelper())->getArrayOfHexColors(0);
     }
 }
