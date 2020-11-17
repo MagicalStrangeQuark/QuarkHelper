d9417529db5edf07057612904178bd417407c41d
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-11-17 18:05:27 -0300

    Parser.php

---
+++ src/CSVHelper/Parser.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Quark\CSVHelper;
+
+final class Parser
+{
+    /**
+     * Constructor.
+     * 
+     * @param void
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * line.
+     * 
+     * @param void
+     */
+    public function line()
+    {
+    }
+
+    /**
+     * toSql.
+     * 
+     * @param void
+     * 
+     * @return string
+     */
+    public function toSql(): array
+    {
+    }
+
+    /**
+     * toJson.
+     * 
+     * @param void
+     * 
+     * @return string
+     */
+    public function toJson(): string
+    {
+    }
+
+    /**
+     * toXml.
+     * 
+     * @param void
+     * 
+     * @return string
+     */
+    public function toXml(): string
+    {
+    }
+}
